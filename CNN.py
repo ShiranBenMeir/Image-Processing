6df2d9a5b82c75753ded0fa9b8ef7197398b87f3
ShiranBenMeir <58989738+ShiranBenMeir@users.noreply.github.com>
2021-08-15 23:08:26 +0300

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -1,7 +1,5 @@
 import math
 import os
-
-import numpy
 import torchvision
 import torch
 import torch.nn as nn
@@ -12,12 +10,11 @@ from sklearn.metrics import roc_curve, roc_auc_score, auc
 from sklearn.preprocessing import LabelBinarizer
 
 criterion = nn.CrossEntropyLoss()
-criterion2 = nn.MSELoss()
 
-target= ['glioma','meningioma','no-tumor','pituitary']
+target = ['glioma', 'meningioma', 'no-tumor', 'pituitary']
 
+fig, c_ax = plt.subplots(1, 1, figsize=(12, 8))
 
-fig, c_ax = plt.subplots(1,1, figsize = (12, 8))
 
 def multiclass_roc_auc_score(y_test, y_pred, average="macro"):
     lb = LabelBinarizer()
@@ -26,10 +23,10 @@ def multiclass_roc_auc_score(y_test, y_pred, average="macro"):
     y_pred = lb.transform(y_pred)
 
     for (idx, c_label) in enumerate(target):
-        fpr, tpr, thresholds = roc_curve(y_test[:,idx].astype(int), y_pred[:,idx])
-        c_ax.plot(fpr, tpr, label = '%s (AUC:%0.2f)'  % (c_label, auc(fpr, tpr)))
-    c_ax.plot(fpr, fpr, 'b-', label = 'Random Guessing')
-    z= roc_auc_score(y_test, y_test, average=average)
+        fpr, tpr, thresholds = roc_curve(y_test[:, idx].astype(int), y_pred[:, idx])
+        c_ax.plot(fpr, tpr, label='%s (AUC:%0.2f)' % (c_label, auc(fpr, tpr)))
+    c_ax.plot(fpr, fpr, 'b-', label='Random Guessing')
+    z = roc_auc_score(y_test, y_test, average=average)
     print('ROC AUC score:', z)
 
     c_ax.legend()
@@ -42,13 +39,14 @@ def multiclass_roc_auc_score(y_test, y_pred, average="macro"):
     print('Outcome values : n', TP, FN, FP, TN)
     print(skm.classification_report(y_test, y_pred))
 
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # 1 input voice channel, 6 output channels, 3x3 square convolution kernel
         self.conv1 = nn.Conv2d(1, 4, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(4, 32, kernel_size=3,padding=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3,padding=1)
+        self.conv2 = nn.Conv2d(4, 32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.conv4 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
 
         self.fc1 = nn.Linear(8192, 4000)
@@ -60,26 +58,26 @@ class Net(nn.Module):
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)),(2,))
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2,))
         x = F.max_pool2d(F.relu(self.conv2(x)), (2,))
         x = F.max_pool2d(F.relu(self.conv3(x)), (2,))
-        #x = F.max_pool2d(F.relu(self.conv4(x)), (2,))
+        # x = F.max_pool2d(F.relu(self.conv4(x)), (2,))
         x = F.relu(self.conv4(x))
-        x = x.view(-1, x.size()[1]*x.size()[2]*x.size()[3])
+        x = x.view(-1, x.size()[1] * x.size()[2] * x.size()[3])
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = F.relu(self.fc2(x))
         x = self.dropout(x)
         x = F.relu(self.fc3(x))
         x = F.relu(self.fc4(x))
-        output = self.fc5(x)
+        output = F.sigmoid(self.fc5(x))
         return output
 
 
-def train(model, train_loader, epochs=20):
+def train(model, train_loader, epochs=3):
     opt = torch.optim.Adam(model.parameters(), lr=0.001)
     model.train()
-    losses=[]
+    losses = []
     for epoch in range(epochs):
         tot_loss = 0
         for batch_idx, (data_x, labels) in enumerate(train_loader):
@@ -91,37 +89,31 @@ def train(model, train_loader, epochs=20):
             tot_loss += loss.item()
             losses.append(loss)
         tot_loss = tot_loss / len(train_loader)
-        print('train loss: ', epoch ," ",tot_loss)
 
     return model
 
 
-
-
-
 def test(model, test_loader):
-    tot_loss=0
-    preds=[]
-    labels=[]
+    tot_loss = 0
+    preds = []
+    labels = []
     with torch.no_grad():
         for batch_idx, (data_x, label) in enumerate(test_loader):
             output = model(data_x)
-            #output = output.max(1, keepdim=True)[1]
-            output=output.type(torch.FloatTensor)
+            # output = output.max(1, keepdim=True)[1]
+            output = output.type(torch.FloatTensor)
             loss = criterion(output, label)
             tot_loss += loss.item()
             output = output.max(1, keepdim=True)[1]
             preds.append(output)
             labels.append(label)
         tot_loss = tot_loss / len(test_loader)
-        print('tot loss: ', tot_loss)
     labels = labels[0].tolist()
     preds = preds[0].tolist()
     preds = [item for sublist in preds for item in sublist]
     accuracy = metrics.accuracy_score(labels, preds)
-    print('accuracy: ',accuracy)
+    print('accuracy: ', accuracy)
     multiclass_roc_auc_score(labels, preds)
-
 
 
 if __name__ == '__main__':
@@ -136,8 +128,6 @@ if __name__ == '__main__':
     train_loader = torch.utils.data.DataLoader(trainData, batch_size=30, shuffle=True)
     val_loader = torch.utils.data.DataLoader(valData, batch_size=30, shuffle=True)
 
-
     model = Net()
-    train_model=train(model, train_loader)
-    preds=test(train_model, val_loader)
-
+    train_model = train(model, train_loader)
+    preds = test(train_model, val_loader)
